241e5ebdda43c90e7c1892810949917783736599
Idowu olamide <idowuolamide81@gmail.com>
2022-06-22 23:45:50 +0100

    update

---
+++ project/index.js
@@ -0,0 +1,43 @@
+// const carousel = new bootstrap.Carousel('#myCarousel');
+
+function Button() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'a00727d195msh12aebc5b2cd51d9p1bab13jsn8b1537a00bde',
+            'X-RapidAPI-Host': 'love-quote.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://love-quote.p.rapidapi.com/lovequote', options)
+    .then((res) => res.json())
+    .then((data)=> {
+        Array.from(data).forEach(function(post) {
+            console.log(data);
+    
+            });
+            document.querySelector('.quote').innerHTML= data.quote
+            document.querySelector('.Author').innerHTML= data.author
+            console.log(data);
+            
+    })
+    .catch(err => console.error(err));
+}
+
+
+
+  
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'a00727d195msh12aebc5b2cd51d9p1bab13jsn8b1537a00bde',
+// 		'X-RapidAPI-Host': 'love-quote.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://love-quote.p.rapidapi.com/lovequote', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
